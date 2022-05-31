7735b44804209476ba089685d8619954ed971366
gabriel_rc__ <ganriel201@gmail.com>
2022-05-31 15:28:28 -0300

    responsividade

--- imports/ui/Tasks.jsx
+++ imports/ui/Tasks.jsx
@@ -8,66 +8,77 @@ import {
   ListItemText,
   ListItemButton,
 } from "@mui/material";
+import { Box } from "@mui/system";
 
 export const Task = ({ task, onDeleteClick, onChangeStatus }) => {
   const navigate = useNavigate();
   return (
     <ListItem
-      className={task.status === "cadastrada" ? "" : `${task.status}`}
+      className={
+        task.status === "cadastrada" ? "" : `${task.status}` + ` task-item`
+      }
       disablePadding
     >
-      <ListItemIcon>
-        <Assignment sx={{ color: "yellow" }} />
-      </ListItemIcon>
-      <ListItemText
-        primary={`${task.name}`}
-        secondary={`autor: ${
-          task.author
-        } | prazo: ${task.date.toLocaleDateString()}`}
-      />
-      <ListItemButton>
-        <Button
-          variant="contained"
-          sx={{ fontSize: 10 }}
-          color="primary"
-          onClick={() => onChangeStatus(task, "cadastrada")}
-        >
-          cadastrada
-        </Button>
-      </ListItemButton>
-      <ListItemButton>
-        <Button
-          disabled={task.status !== "cadastrada"}
-          variant={task.status === "em-andamento" ? "outlined" : "contained"}
-          sx={{ fontSize: 10 }}
-          color="warning"
-          onClick={() => onChangeStatus(task, "em-andamento")}
-        >
-          em andamento
-        </Button>
-      </ListItemButton>
-      <ListItemButton>
-        <Button
-          disabled={task.status !== "em-andamento"}
-          variant={task.status === "comcluida" ? "outlined" : "contained"}
-          sx={{ fontSize: 10 }}
-          color="success"
-          onClick={() => onChangeStatus(task, "comcluida")}
-        >
-          comcluida
-        </Button>
-      </ListItemButton>
-      <ListItemIcon>
-        <ListItemButton
-          edg="end"
-          onClick={() => navigate("/EditTask", { state: { task } })}
-        >
-          <Edit />
+      <Box>
+        <ListItemIcon>
+          <Assignment sx={{ color: "yellow" }} />
+        </ListItemIcon>
+        <ListItemText
+          primary={`${task.name}`}
+          secondary={`autor: ${
+            task.author
+          } | prazo: ${task.date.toLocaleDateString()}`}
+        />
+      </Box>
+      <Box>
+        <ListItemButton>
+          <Button
+            variant="contained"
+            sx={{ fontSize: 10 }}
+            color="primary"
+            onClick={() => onChangeStatus(task, "cadastrada")}
+          >
+            cadastrada
+          </Button>
         </ListItemButton>
-        <ListItemButton edge="end" onClick={() => onDeleteClick(task)}>
-          <Delete sx={{ color: "red" }} />
+        <ListItemButton>
+          <Button
+            disabled={task.status !== "cadastrada"}
+            variant={task.status === "em-andamento" ? "outlined" : "contained"}
+            sx={{ fontSize: 10 }}
+            color="warning"
+            onClick={() => onChangeStatus(task, "em-andamento")}
+          >
+            em andamento
+          </Button>
         </ListItemButton>
-      </ListItemIcon>
+        <ListItemButton>
+          <Button
+            disabled={task.status !== "em-andamento"}
+            variant={task.status === "comcluida" ? "outlined" : "contained"}
+            sx={{ fontSize: 10 }}
+            color="success"
+            onClick={() => onChangeStatus(task, "comcluida")}
+          >
+            comcluida
+          </Button>
+        </ListItemButton>
+      </Box>
+      <Box>
+        <ListItemIcon>
+          <ListItemButton
+            edg="end"
+            onClick={() => navigate("/EditTask", { state: { task } })}
+          >
+            <Edit />
+          </ListItemButton>
+        </ListItemIcon>
+        <ListItemIcon>
+          <ListItemButton edge="end" onClick={() => onDeleteClick(task)}>
+            <Delete sx={{ color: "red" }} />
+          </ListItemButton>
+        </ListItemIcon>
+      </Box>
     </ListItem>
   );
 };
